146ae31c8fe216940d16d848668ee33314bb62e8
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-04-06 19:49:35 +0200

    fix(link): fix stories display

--- src/navigation/Link/Link.tsx
+++ src/navigation/Link/Link.tsx
@@ -1,35 +1,42 @@
-import React from 'react'
-import styles from './Link.module.css'
-import { Plus } from '../../icons'
+import React from "react";
+import styles from "./Link.module.css";
+import { Plus } from "../../icons";
 
 interface LinkProps {
-	to: string
-	id?: string
-	target?: '_blank' | '_self' | '_parent' | '_top'
-	disabled?: boolean
-	onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
-	children: React.ReactNode
+  to: string;
+  id?: string;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  disabled?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+  children: React.ReactNode;
 }
 
 const Link: React.FC<LinkProps> = ({
-	to = 'www.google.pl',
-	id = 'google',
-	target = '_blank',
-	disabled = false,
-	onClick,
-	children,
+  to = "www.google.pl",
+  id = "google",
+  target = "_blank",
+  disabled = false,
+  onClick,
+  children,
 }) => {
-	const globalLinkClass = styles.linkGlobal
-	const optionClass = disabled === false ? styles.linkEnabled : styles.linkDisabled
-	const allClasses = [globalLinkClass, optionClass].join(' ')
-	return (
-		<a className={allClasses} href={to} id={id} target={target} onClick={onClick}>
-			<span className={styles.iconSettings}>
-				<Plus />
-			</span>
-			{children}
-		</a>
-	)
-}
+  const globalLinkClass = styles.linkGlobal;
+  const optionClass =
+    disabled === false ? styles.linkEnabled : styles.linkDisabled;
+  const allClasses = [globalLinkClass, optionClass].join(" ");
+  return (
+    <a
+      className={allClasses}
+      href={to}
+      id={id}
+      target={target}
+      onClick={onClick}
+    >
+      <span className={styles.iconSettings}>
+        <Plus />
+      </span>
+      {children}
+    </a>
+  );
+};
 
-export default Link
+export default Link;
