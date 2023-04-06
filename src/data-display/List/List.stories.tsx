9b80446b41d17d4e0507f0ff6e51b53694f8eb25
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-04-07 00:43:41 +0200

    feat(list): add list

---
+++ src/data-display/List/List.stories.tsx
@@ -0,0 +1,81 @@
+import { Meta, Story } from "@storybook/react";
+import styles from "./List.module.css";
+import { List, ListItem } from "./List";
+import React, { Fragment } from "react";
+import { Switch } from "../../inputs";
+import Checkbox from "../../inputs/Checkbox/Checkbox";
+
+interface ListItemArgs {
+  header: string;
+  supportText: string;
+  className: string;
+}
+
+export default {
+  component: List,
+} as Meta;
+
+export const Default: Story<ListItemArgs> = (args) => (
+  <List>
+    <ListItem>
+      <div>
+        <h3 className={styles.header}>{args.header}</h3>
+        <p className={styles.supportText}>{args.supportText}</p>
+      </div>
+    </ListItem>
+  </List>
+);
+
+Default.args = {
+  header: "List Item",
+  supportText: "Supporting line text lorem ipsum dolor sit amet, consectetur",
+};
+
+export const ListItemSwitch: Story<ListItemArgs> = (args) => (
+  <>
+    <List>
+      <ListItem className={styles.listItem}>
+        <div className={styles.switchContainer}>
+          <Switch label={""} id={""} />
+        </div>
+        <div>
+          <h3 className={styles.header}>{args.header}</h3>
+          <p className={styles.supportText}>{args.supportText}</p>
+        </div>
+      </ListItem>
+    </List>
+  </>
+);
+
+ListItemSwitch.args = {
+  header: "List Item",
+  supportText: "Supporting line text lorem ipsum dolor sit amet, consectetur",
+};
+
+export const ListItemCheckbox: Story<ListItemArgs> = (args) => (
+  <>
+    <List>
+      <ListItem className={styles.listItem}>
+        <div>
+          <h3 className={styles.header}>{args.header}</h3>
+          <p className={styles.supportText}>{args.supportText}</p>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <Checkbox
+            id={""}
+            name={""}
+            value={""}
+            label={"Checkboc label"}
+            checked={false}
+            disabled={false}
+          />
+        </div>
+      </ListItem>
+    </List>
+  </>
+);
+
+ListItemCheckbox.args = {
+  header: "List Item",
+  supportText: "Supporting line text lorem ipsum dolor sit amet, consectetur",
+};
