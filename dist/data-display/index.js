28408fb4902733718661efa1b54d5d237084a761
Jakub Wąsowski <jakub@nerdz.pl>
2023-04-28 17:26:30 +0200

    chore: fixes, storybook bump, tests fixes

--- dist/data-display/index.js
+++
@@ -1,2 +0,0 @@
-export { Avatar } from "./Avatar/Avatar";
-//# sourceMappingURL=index.js.map
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./inputs";
-export * from "./navigation";
-export * from "./data-display";
-//# sourceMappingURL=index.js.map
