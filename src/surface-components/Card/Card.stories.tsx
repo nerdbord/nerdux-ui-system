295819d912b0264c2ac33349cc6287c8c26e9c7c
Hubert <h.kwiatkowski@hotmail.com>
2023-05-18 19:33:57 +0200

    fix: fixed feedback issues

--- src/surface-components/Card/Card.stories.tsx
+++ src/surface-components/Card/Card.stories.tsx
@@ -16,41 +16,34 @@ interface CardStoryProps {
 export const Default: React.FC<InferProps<CardStoryProps>> = ({
   fullWidth,
 }) => {
-  const handlePrimaryButtonClick = () => {
-    console.log("Primary button clicked");
-  };
-
-  const handleSecondaryButtonClick = () => {
-    console.log("Secondary button clicked");
-  };
-
-  const labels = ["Click!", "DO NOT CLICK ME!"];
-
   return (
     <Card fullWidth={fullWidth}>
       <CardImage url={imageUrl} alt={"default alt"} />
       <CardContent>
         <h3>Card Name</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-          architecto, autem distinctio dolorem, dolores, esse facere incidunt
-          odio officia quasi similique sunt totam unde ut veritatis. A, amet
-          aperiam architecto assumenda aut autem commodi consequatur deleniti
-          dolorem enim esse excepturi harum illum iure labore minus, modi
-          molestiae natus nesciunt non recusandae rem saepe sapiente sint
-          temporibus ut vero voluptate. Consequuntur ducimus eius et praesentium
-          recusandae. Dolore eaque eius eum excepturi expedita in ipsum laborum
-          maiores necessitatibus perspiciatis? Autem beatae, culpa delectus
-          dignissimos, dolorem doloremque eaque hic nemo nobis omnis possimus
-          quae sint, sit vel veniam! Dignissimos doloribus laudantium provident
-          veritatis.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          aspernatur autem culpa est expedita hic libero provident quis sequi
+          vitae!
         </p>
-        <Button onClick={handlePrimaryButtonClick} variant={"primary"}>
-          {labels[0]}
-        </Button>
-        <Button onClick={handleSecondaryButtonClick} variant={"secondary"}>
-          {labels[1]}
-        </Button>
+        <div>
+          <Button
+            onClick={() => {
+              console.log("Primary button clicked");
+            }}
+            variant={"primary"}
+          >
+            Primary button
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("Secondary button clicked");
+            }}
+            variant={"secondary"}
+          >
+            Secondary button
+          </Button>
+        </div>
       </CardContent>
     </Card>
   );
